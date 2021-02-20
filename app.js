79debb4499e873c37ca3e1d070cabb00a2a79664
stanregie105 <stanleyregal004@yahoo.com>
2021-02-20 12:41:56 -0800

    little tweak on app.js

--- app.js
+++ app.js
@@ -82,6 +82,9 @@ if (process.env.NODE_ENV == "production") {
   });
 }
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "client/public/index.html"));
+});
 
 
 app.use(globalErrorHandler);
