b08ff43fab9c46b4d77e3e117593c4a86087b859
stanregie105 <stanleyregal004@yahoo.com>
2021-02-23 04:27:37 -0800

    modified logistics

--- client/src/components/logistics/logistics.component.jsx
+++ client/src/components/logistics/logistics.component.jsx
@@ -12,7 +12,12 @@ const Logistic=()=>(
       <div className='col-sm-6'>
          <img src="/img/trailer2c.jpg"/>
       </div>
+    </div>
+    <div className='row'>
+      <div className='col-12'>
       <p>We are fully on site ready to shuttle your goods and crude oil from any location within the country. We are ever ready to serve you with our trucks which are very efficient and duely reliable. </p>
+
+      </div>
     </div>
     <h2>Air Freight</h2>
     <div className='row apartment'>
@@ -23,7 +28,12 @@ const Logistic=()=>(
       <div className='col-sm-6'>
          <img src="/img/air2.jpg"/>
       </div>
+    </div>
+    <div className='row'>
+      <div className='col-12'>
       <p>We also move your goods from every part of the world with our flights. We are ever ready to serve you from every point of call,often in a timely manner. </p>
+
+      </div>
     </div>
     <h2>Cargo Freight</h2>
     <div className='row apartment'>
@@ -34,8 +44,13 @@ const Logistic=()=>(
     <div className='col-sm-6'>
        <img src="/img/oilship.jpg"/>
     </div>
-    <p>Our ships are fully functional and reliable to deliver your products/crude oil from any part of the world. Your consignment is duely safe with us.</p>
   </div>
+  <div className='row'>
+      <div className='col-12'>
+      <p>Our ships are fully functional and reliable to deliver your products/crude oil from any part of the world. Your consignment is duely safe with us.</p>
+
+      </div>
+    </div>
 </React.Fragment>
 );
 
