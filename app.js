5ea017ff5a57815911ed3fa5573fef72e82c68c6
stanregie105 <stanleyregal004@yahoo.com>
2021-03-01 02:24:24 -0800

    static reformed

--- app.js
+++ app.js
@@ -23,20 +23,20 @@ const app = express();
 app.enable("trust proxy");
 
 // Serve static files from the React app
-
+/*
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  //app.get("*", (req, res) => {
-   // res.sendFile(path.join(__dirname, "client/build", "index.html"));
- // });
+  app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "client/public/index.html"));
 });
 
-/*
+*/
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   // Serving static files
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
   //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   // });
 }
-*/
+
 
 
 app.use(compression());
