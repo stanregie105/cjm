99f9e506031c347369b3233620128c89c37a96aa
stanregie105 <stanleyregal004@yahoo.com>
2021-03-01 01:28:22 -0800

    build change

--- app.js
+++ app.js
@@ -22,6 +22,19 @@ const app = express();
 
 app.enable("trust proxy");
 
+// Serve static files from the React app
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "client/public/index.html"));
+});
 /*
 if (process.env.NODE_ENV === "production") {
   // Set static folder
@@ -85,19 +98,6 @@ app.use("/api/v1/vehicles", vehicleRoute);
 app.use("/api/v1/order", orderRoute);
 app.use("/api/v1/talk", talkRoute);
 
-// Serve static files from the React app
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "client/public/index.html"));
-});
 
 app.use(globalErrorHandler);
 
