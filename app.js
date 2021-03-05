a9ad5ecbc8d9ef6351f9026e3d565a4cd1889427
stanregie105 <stanleyregal004@yahoo.com>
2021-03-05 04:10:44 -0800

    new static

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "client/public/index.html"));
 });
 
-*/
+
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   // Serving static files
@@ -46,6 +46,14 @@ if (process.env.NODE_ENV === "production") {
    //app.get("*", (req, res) => {
     //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
    //});
+}
+*/
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 
