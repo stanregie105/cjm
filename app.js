04446ac669d6a8ddb70dfa2ded261f4a5477bd96
stanregie105 <stanleyregal004@yahoo.com>
2021-03-05 04:23:37 -0800

    after route

--- app.js
+++ app.js
@@ -35,26 +35,11 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "client/public/index.html"));
 });
-
-
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // Serving static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // app.use(express.static("client/build"));
-  app.use('*', express.static(path.join(__dirname, "client", "build")));
-   //app.get("*", (req, res) => {
-    //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-   //});
-}
 */
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+
+
+
 
 
 
@@ -107,7 +92,16 @@ app.use("/api/v1/vehicles", vehicleRoute);
 app.use("/api/v1/order", orderRoute);
 app.use("/api/v1/talk", talkRoute);
 
-
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  // Serving static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // app.use(express.static("client/build"));
+  app.use('*', express.static(path.join(__dirname, "client", "build")));
+   //app.get("*", (req, res) => {
+    //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   //});
+}
 app.use(globalErrorHandler);
 
 module.exports = app;
